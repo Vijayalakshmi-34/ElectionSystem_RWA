1eb317a48c3733052a88cb8586445f288844bfdf
Anusha Dadi <anusha.dadi@outlook.com>
2019-12-04 11:44:53 +0530

    update-unit

---
+++ ElectionSystem_RWA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ElectionSystem_RWA.MvcApplication" Language="C#" %>
---
+++ ElectionSystem_RWA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElectionSystem_RWA.Startup))]
+namespace ElectionSystem_RWA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
